678411f84afad207ba4793a797331300f1f29e16
donaldxdonald <mzhlovefama@gmail.com>
2025-11-29 00:42:27 +0800

    chore: update to Angular v21

--- projects/embla-carousel-angular/src/public-api.ts
+++ projects/embla-carousel-angular/src/public-api.ts
@@ -2,11 +2,18 @@
  * Public API Surface of embla-carousel-angular
  */
 
-export {
+export type {
   EmblaCarouselType,
   EmblaEventType,
   EmblaOptionsType,
   EmblaPluginType,
+  EmblaEventListType,
+  EmblaPluginsType,
+  EngineType,
+  ScrollBodyType,
+  CreatePluginType,
+  CreateOptionsType,
+  OptionsHandlerType,
 } from 'embla-carousel'
 export { EmblaCarouselDirective } from './lib/embla-carousel.directive'
 export { EMBLA_OPTIONS_TOKEN, provideEmblaGlobalOptions } from './lib/utils'
